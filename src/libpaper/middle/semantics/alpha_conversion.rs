a58f620ab5929cc44c1d98f99efca874ff61b13f
Pierre Talbot <ptalbot@hyc.io>
2015-02-02 02:10:44 +0100

    Alpha conversion of the entire program.

--- src/libpaper/middle/semantics/alpha_conversion.rs
+++ src/libpaper/middle/semantics/alpha_conversion.rs
@@ -12,13 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use front::ast::{Program, Expr};
 use front::constraint_ast::Constraint;
 use middle::semantics::symbol_table::SymbolTable;
 
 trait AlphaConversion
 {
   type TargetAST;
-  fn convert(self, symbols: &SymbolTable) -> Self::TargetAST;
+  fn rename(self, symbols: &SymbolTable) -> Self::TargetAST;
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -33,12 +34,92 @@ impl AlphaConversion for Constraint
 {
   type TargetAST = AConstraint;
 
-  fn convert(self, symbols: &SymbolTable) -> AConstraint {
+  fn rename(self, symbols: &SymbolTable) -> AConstraint {
     match self {
       Constraint::XEqualY(x, y) => AConstraint::XEqualY(symbols[x], symbols[y]),
       Constraint::XEqualC(x, c) => AConstraint::XEqualC(symbols[x], c),
       Constraint::XNotEqualY(x, y) => AConstraint::XNotEqualY(symbols[x], symbols[y]),
       Constraint::XLessThanY(x, y) => AConstraint::XLessThanY(symbols[x], symbols[y])
+    }
+  }
+}
+
+pub struct AProgram<C, D>
+{
+  pub args: Vec<u32>,
+  pub def: AExpr<C, D>
+}
+
+pub enum AExpr<C, D>
+{
+  ReactiveSum(Vec<AExpr<C, D>>),
+  Parallel(Vec<AExpr<C, D>>),
+  Replicate(Box<AExpr<C, D>>),
+  When(C, Box<AExpr<C, D>>),
+  Unless(C, Box<AExpr<C, D>>),
+  Tell(C),
+  Next(Box<AExpr<C, D>>),
+  Async(Box<AExpr<C, D>>),
+  LetIn(u32, D, Box<AExpr<C, D>>)
+}
+
+fn alpha_conversion<C, D>(program: Program<C,D>)
+  -> AProgram<<C as AlphaConversion>::TargetAST, D>
+  where C: AlphaConversion
+{
+  let mut symbol_table = SymbolTable::new();
+  AProgram {
+    args: program.args.into_iter()
+      .map(|arg| symbol_table.shadow(arg))
+      .collect(),
+    def: alpha_expr(&mut symbol_table, program.def)
+  }
+}
+
+fn alpha_expr<C, D>(symbol_table: &mut SymbolTable, expr: Expr<C,D>)
+  -> AExpr<<C as AlphaConversion>::TargetAST, D>
+  where C: AlphaConversion
+{
+  match expr {
+    Expr::ReactiveSum(exprs) => {
+      AExpr::ReactiveSum(exprs.into_iter()
+        .map(|expr| alpha_expr(symbol_table, expr))
+        .collect())
+    }
+    Expr::Parallel(exprs) => {
+      AExpr::Parallel(exprs.into_iter()
+        .map(|expr| alpha_expr(symbol_table, expr))
+        .collect())
+    }
+    Expr::Replicate(expr) => {
+      AExpr::Replicate(box alpha_expr(symbol_table, *expr))
+    }
+    Expr::When(c, expr) => {
+      AExpr::When(
+        c.rename(symbol_table),
+        box alpha_expr(symbol_table, *expr)
+      )
+    }
+    Expr::Unless(c, expr) => {
+      AExpr::Unless(
+        c.rename(symbol_table),
+        box alpha_expr(symbol_table, *expr)
+      )
+    },
+    Expr::Tell(c) => {
+      AExpr::Tell(c.rename(symbol_table))
+    }
+    Expr::Next(expr) => {
+      AExpr::Next(box alpha_expr(symbol_table, *expr))
+    }
+    Expr::Async(expr) => {
+      AExpr::Async(box alpha_expr(symbol_table, *expr))
+    }
+    Expr::LetIn(name, dom, expr) => {
+      let uid = symbol_table.shadow(name.clone());
+      let expr = box alpha_expr(symbol_table, *expr);
+      symbol_table.unshadow(name);
+      AExpr::LetIn(uid, dom, expr)
     }
   }
 }
